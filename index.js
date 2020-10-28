cdcfe944c7b23ea1a0c341fb1d53b985759a2d2f
spagnon7 <spagnon7@gmail.com>
2020-10-27 21:25:44 -0700

    finished

---
+++ index.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+document.querySelector('img').setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+
+document.querySelector('img').alt = 'chicken';
